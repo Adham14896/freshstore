1789234d19608dc51156a09e148791b79dcc6f83
Adham14896 <teenadham96@gmail.com>
2024-09-16 16:19:40 +0300

    2nd login and logout fix

--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
+import { UserContext } from "../../Context/UserContext";
 
 export default function ProtectedRoute({ children }) {
-  if (localStorage.getItem("userToken")) {
+  const { isLoggedIn } = useContext(UserContext);
+
+  if (localStorage.getItem("userToken") && isLoggedIn) {
     return children;
   } else {
     return <Navigate to={"/login"} />;
